94c0636d12a72a224113ec6a1017f4dbdea13a95
James Gilbrook <james@gilbrook.ca>
2012-09-07 22:19:56 -0400

    Add unittest for command registration

--- tests/test_application_basic.py
+++ tests/test_application_basic.py
@@ -35,5 +35,70 @@ class TestCmdrAttrs(unittest.TestCase):
         self.assertEqual(self.app.exit_msg, self.data['exit_msg'])
 
 
+class TestCmdrCommandRegistration(unittest.TestCase):
+    def test_register_as_args(self):
+
+        num_cmds = 10
+        cmd_list = generate_cmd_helper(num_cmds)
+        app = cmdr.Cmdr(__name__, registered_commands=cmd_list)
+    
+        # Consider the builting commands
+        self.assertEqual(len(app.registered_cmds), num_cmds+2)
+
+        del cmd_list
+        del app
+
+    def test_builtin_cmds(self):
+
+        app = cmdr.Cmdr(__name__)
+        
+        self.assertEqual(len(app.registered_cmds), 2)
+
+        cmd_names = [c.name for c in app.registered_cmds]
+        self.assertIn("help", cmd_names)
+        self.assertIn("exit", cmd_names)
+
+        del app
+
+    def test_register_func(self):
+
+        num_cmds = 50
+        c_list = generate_cmd_helper(num_cmds)
+        app = cmdr.Cmdr(__name__)
+
+        for c in c_list:
+            app.register_cmd(c)
+
+        self.assertEqual(len(app.registered_cmds), num_cmds+2)
+
+        del c_list
+        del app
+
+
+    def test_register_decorator(self):
+
+        app = cmdr.Cmdr(__name__)
+
+        @app.cmd("test")
+        def test():
+            pass
+
+        self.assertEqual(len(app.registered_cmds), 3)
+
+        del app
+
+# Helper functions
+def generate_cmd_helper(num):
+    
+    c_list = []
+    name_prefix = "TestCmd%s"
+    for i in xrange(num):
+        name = name_prefix % i
+        c_list.append(cmdr.Command(name))
+
+    return c_list
+        
+
+
 if __name__ == '__main__':
     unittest.main()
