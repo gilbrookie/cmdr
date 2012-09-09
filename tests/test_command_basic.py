fee4e7f94eb5fec6b9c80b2698f8cfda506574fd
James Gilbrook <james@gilbrook.ca>
2012-09-08 22:00:35 -0400

    Modify the Command interface - remove validate() and valid_func(), + minor fixes
    I haven't really come up with a great reason to include the validation code inside the Command
    class.  I initially wanted it to be called before a execution is called, and the validation would
    provide a way to validate the command arguments.
    
    However, as I have been using the code, this seems overkill and a waste.  If I really needed
    validation, I'd just write it myself in the execution code anyways.
    
    So the validation code is now removed.
    
    **Possible Risky change**:
    Modified logic in the Command __init__ to fix issues with assigning the execution function.
    This should ONLY apply if creating a Command object directly or using the Cmdr.cmd decorator.
    Subclassing Command *should* not be affected - but I need to write the unittest to verify.
    
    The issue came up in the unittest where no exec_func argument was provided and a subcommand was
    detected.  In this case the sub command got exec_func=None (which was wrong)
    New logic:
    - if single cmd && no exec_func : use default
    - if subcmd && no exec_func: both use default
    - if single cmd && exec_func: use exec_func
    - if subcmd && exec_func: subcmd use exec_func, primary cmd uses default.
    * This is what I have originally in mind, but the code was broken.
    ** Unittest added to verify this functionality
    
    Additional changes:
    - Added more tests for direct Command objects

--- tests/test_command_basic.py
+++ tests/test_command_basic.py
@@ -19,10 +19,20 @@ class TestCmd2(cmdr.Command):
         return "override execute"
 
 class TestCmd3(cmdr.Command):
+    @cmdr.subcmd
     def sub1(self, *args):
         pass
 
+    @cmdr.subcmd
     def sub2(self):
+        pass
+
+class TestCmd4(cmdr.Command):
+    @cmdr.subcmd
+    def sub1(self):
+        pass
+
+    def execute(self):
         pass
 
 ## End Test Data ##
@@ -31,34 +41,96 @@ class TestCmd3(cmdr.Command):
 ## Test Classes ##
 
 class TestCommandBasic(unittest.TestCase):
-    def load_data():
+    def load_data(self):
         self.data={}
         self.data['name'] = self.__class__.__name__
         self.data['alt']= u'alt'
         self.data['exec_func'] = func_w_args
         self.data['description'] = u"Test description"
 
-    def test_args():
-        pass
+    def setUp(self):
+        self.load_data()
+        self.cmd = cmdr.Command(cmd=self.data['name'],
+                                alt=self.data['alt'],
+                                exec_func=self.data['exec_func'],
+                                description=self.data['description'])
 
-    def test_cmd_name():   
-        pass
+    def tearDown(self):
+        self.cmd = None
 
-    def test_cmd_dict():
-        pass
+    #def test_args(self):
+    #    self.assertEqual(self.cmd.alt, self.data['alt'])
 
-    def test_cmd_strs():
-        pass
+    def test_cmd_name(self):   
+        self.assertEqual(self.cmd.name, self.data['name'])
 
-    def test_execution_default():
-        pass
+    def test_cmd_description(self):
+        self.assertEqual(self.cmd.description, self.data['description'])
 
-    def test_execution_callback():
-        pass
+    def test_cmd_exec_func(self):
+        self.assertEqual(self.cmd.exec_func, self.data['exec_func'])
 
+    def test_cmd_dict(self):
+        c_dict = self.cmd.cmd_dict
+        self.assertIn(self.data['name'], c_dict)
+        self.assertEqual(self.data['description'],
+                         c_dict[self.data['name']]['help'])
+        self.assertEqual(self.data['alt'],
+                         c_dict[self.data['name']]['alt'])
+        self.assertEqual(self.data['exec_func'],
+                         c_dict[self.data['name']]['exec_func'])
 
+        self.assertEqual(None,
+                         c_dict[self.data['name']]['comp_dict'])
+        self.assertEqual({},
+                         c_dict[self.data['name']]['subcmds'])
+
+    def test_cmd_strs(self):
+        self.assertEqual([self.data['name']],
+                         self.cmd.cmd_strs)
+
+        
 class TestDirectCommand(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.cmd1 = cmdr.Command(cmd="Test")
+        self.cmd2 = cmdr.Command(cmd="Test sub")
+        self.cmd3 = cmdr.Command(cmd="Test", exec_func=func_w_args)
+        self.cmd4 = cmdr.Command(cmd="Test sub", exec_func=func_no_args)
+
+    def tearDown(self):
+        self.cmd1 = None
+        self.cmd2 = None
+        self.cmd3 = None
+        self.cmd4 = None
+
+    def test_cmd_name(self):
+        # the case for cmd1 covered in TestCommandBasic
+        self.assertEqual("Test", self.cmd1.name)
+        self.assertEqual("Test", self.cmd2.name)
+        self.assertEqual("Test", self.cmd3.name)
+        self.assertEqual("Test", self.cmd4.name)
+    
+    def test_subcmd_name(self):
+        self.assertEqual({}, self.cmd1.subcmds)
+        self.assertIn("sub", self.cmd2.subcmds)
+        self.assertEqual({}, self.cmd3.subcmds)
+        self.assertIn("sub", self.cmd4.subcmds)
+
+    def test_exec_func(self):
+        self.assertEqual(self.cmd1.exec_func,
+                         self.cmd1.execute)
+        self.assertEqual(self.cmd2.exec_func,
+                         self.cmd2.execute)
+        self.assertEqual(func_w_args,
+                         self.cmd3.exec_func)
+
+        self.assertEqual(self.cmd4.exec_func,
+                         self.cmd4.execute)
+
+        # Test the subcmd got assigned the exec_func
+        self.assertEqual(func_no_args,
+                         self.cmd4.subcmds["sub"]['exec_func'])
+
 
 class TestSubclassCommand(unittest.TestCase):
     pass
