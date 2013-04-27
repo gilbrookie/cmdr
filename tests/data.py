7489116ed8211f7532b4d9419b5eba7568e47520
James Gilbrook <james@gilbrook.ca>
2013-04-27 15:30:00 -0400

    Fix some print statements in the test data to make them python3 compatible.

--- tests/data.py
+++ tests/data.py
@@ -39,12 +39,12 @@ class TestCmd3(cmdr.Command):
     @cmdr.subcmd
     def sub1(self, *args):
         """Test sub1"""
-        print "Test sub1 %s" % " ".join(*args)
+        print("Test sub1 %s" % ' '.join(*args))
 
     @cmdr.subcmd
     def sub2(self):
         """Test sub2"""
-        print "Test sub2"
+        print("Test sub2")
 
 
 class TestCmd4(cmdr.Command):
@@ -78,14 +78,14 @@ app = cmdr.Cmdr("CmdrDecoratedCmds")
 @app.cmd('no_args')
 def decorated_no_args():
     """Decorated command with no arguments"""
-    print "decorated_no_args"
+    print("decorated_no_args")
 
 
 @app.cmd('with_args')
 def decorated_w_args(*args):
     """Decorated command with arguments"""
 
-    print "decorated_w_args %s" % ",".join(*args)
+    print("decorated_w_args %s" % ",".join(*args))
 
 # rename the app to a more descriptive name
 CmdrDecoratedCmds = app
